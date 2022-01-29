94156278d48205c20606d97d475e7acfb3f65913
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-29 12:10:32 -0500

    commit tree df66253e75603c2e6e2c842280cd38000303e98f

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1*num2);
+const random = Math.floor(Math.random()*100)+1;
+const num3 = 25;
+const num4 = 7;
+const mod = 25%7;
+const max = Math.max(2,5,13,20,18,3);
